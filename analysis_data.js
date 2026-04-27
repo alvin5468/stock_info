229ff2f580fd80cd51399827d94d6aebaa3faeef
Alvin Chou <choualvin@example.com>
2026-04-28 00:36:44 +0800

    add layout and color, add description

--- analysis_data.js
+++ analysis_data.js
@@ -8,87 +8,15 @@ const ANALYSIS_DATA = {
       "type": "etf"
     },
     {
-      "code": "2330",
-      "name": "台積電",
-      "return": 4471.1,
-      "type": "current"
-    },
-    {
-      "code": "2308",
-      "name": "台達電",
-      "return": 5010.8,
-      "type": "current"
-    },
-    {
-      "code": "2317",
-      "name": "鴻海",
-      "return": 84.6,
-      "type": "current"
-    },
-    {
-      "code": "2454",
-      "name": "聯發科",
-      "return": 754.4,
-      "type": "current"
-    },
-    {
-      "code": "3711",
-      "name": "日月光投控",
-      "return": 519.2,
-      "type": "current"
-    },
-    {
-      "code": "2891",
-      "name": "中信金",
-      "return": 86.9,
-      "type": "current"
-    },
-    {
-      "code": "2345",
-      "name": "智邦",
-      "return": 5928.2,
-      "type": "current"
-    },
-    {
       "code": "2383",
       "name": "台光電",
       "return": 62052.8,
       "type": "current"
     },
     {
-      "code": "2382",
-      "name": "廣達",
-      "return": 466.7,
-      "type": "current"
-    },
-    {
-      "code": "2881",
-      "name": "富邦金",
-      "return": 218.1,
-      "type": "current"
-    },
-    {
-      "code": "2882",
-      "name": "國泰金",
-      "return": 103.0,
-      "type": "current"
-    },
-    {
-      "code": "2303",
-      "name": "聯電",
-      "return": 253.1,
-      "type": "current"
-    },
-    {
-      "code": "3017",
-      "name": "奇鋐",
-      "return": 7451.3,
-      "type": "current"
-    },
-    {
-      "code": "3037",
-      "name": "欣興",
-      "return": 3951.3,
+      "code": "2368",
+      "name": "金像電",
+      "return": 23066.7,
       "type": "current"
     },
     {
@@ -98,27 +26,189 @@ const ANALYSIS_DATA = {
       "type": "current"
     },
     {
-      "code": "2887",
-      "name": "台新新光金",
-      "return": 34.6,
+      "code": "3017",
+      "name": "奇鋐",
+      "return": 7451.3,
       "type": "current"
     },
     {
-      "code": "2412",
-      "name": "中華電",
-      "return": 163.1,
+      "code": "2345",
+      "name": "智邦",
+      "return": 5928.2,
       "type": "current"
     },
     {
-      "code": "2884",
-      "name": "玉山金",
-      "return": 114.1,
+      "code": "2308",
+      "name": "台達電",
+      "return": 5010.8,
+      "type": "current"
+    },
+    {
+      "code": "3661",
+      "name": "世芯-KY",
+      "return": 4689.8,
+      "type": "current"
+    },
+    {
+      "code": "2330",
+      "name": "台積電",
+      "return": 4471.1,
+      "type": "current"
+    },
+    {
+      "code": "3653",
+      "name": "健策",
+      "return": 4030.8,
+      "type": "current"
+    },
+    {
+      "code": "3037",
+      "name": "欣興",
+      "return": 3951.3,
+      "type": "current"
+    },
+    {
+      "code": "2059",
+      "name": "川湖",
+      "return": 3258.4,
+      "type": "current"
+    },
+    {
+      "code": "2327",
+      "name": "國巨*",
+      "return": 3200.0,
+      "type": "current"
+    },
+    {
+      "code": "2449",
+      "name": "京元電子",
+      "return": 1721.7,
+      "type": "current"
+    },
+    {
+      "code": "2207",
+      "name": "和泰車",
+      "return": 1637.7,
+      "type": "current"
+    },
+    {
+      "code": "3008",
+      "name": "大立光",
+      "return": 1199.0,
+      "type": "current"
+    },
+    {
+      "code": "6669",
+      "name": "緯穎",
+      "return": 1152.7,
+      "type": "current"
+    },
+    {
+      "code": "2408",
+      "name": "南亞科",
+      "return": 895.2,
+      "type": "current"
+    },
+    {
+      "code": "2357",
+      "name": "華碩",
+      "return": 859.0,
+      "type": "current"
+    },
+    {
+      "code": "2603",
+      "name": "長榮",
+      "return": 832.9,
+      "type": "current"
+    },
+    {
+      "code": "2454",
+      "name": "聯發科",
+      "return": 754.4,
+      "type": "current"
+    },
+    {
+      "code": "1216",
+      "name": "統一",
+      "return": 562.0,
+      "type": "current"
+    },
+    {
+      "code": "3711",
+      "name": "日月光投控",
+      "return": 519.2,
+      "type": "current"
+    },
+    {
+      "code": "2344",
+      "name": "華邦電",
+      "return": 488.0,
+      "type": "current"
+    },
+    {
+      "code": "2382",
+      "name": "廣達",
+      "return": 466.7,
+      "type": "current"
+    },
+    {
+      "code": "2395",
+      "name": "研華",
+      "return": 466.4,
       "type": "current"
     },
     {
       "code": "2885",
       "name": "元大金",
       "return": 409.7,
+      "type": "current"
+    },
+    {
+      "code": "2301",
+      "name": "光寶科",
+      "return": 333.5,
+      "type": "current"
+    },
+    {
+      "code": "3231",
+      "name": "緯創",
+      "return": 313.7,
+      "type": "current"
+    },
+    {
+      "code": "3045",
+      "name": "台灣大",
+      "return": 289.7,
+      "type": "current"
+    },
+    {
+      "code": "4904",
+      "name": "遠傳",
+      "return": 274.4,
+      "type": "current"
+    },
+    {
+      "code": "2303",
+      "name": "聯電",
+      "return": 253.1,
+      "type": "current"
+    },
+    {
+      "code": "2881",
+      "name": "富邦金",
+      "return": 218.1,
+      "type": "current"
+    },
+    {
+      "code": "1303",
+      "name": "南亞",
+      "return": 186.3,
+      "type": "current"
+    },
+    {
+      "code": "2412",
+      "name": "中華電",
+      "return": 163.1,
       "type": "current"
     },
     {
@@ -134,123 +224,27 @@ const ANALYSIS_DATA = {
       "type": "current"
     },
     {
-      "code": "2357",
-      "name": "華碩",
-      "return": 859.0,
+      "code": "2884",
+      "name": "玉山金",
+      "return": 114.1,
       "type": "current"
     },
     {
-      "code": "3231",
-      "name": "緯創",
-      "return": 313.7,
+      "code": "2882",
+      "name": "國泰金",
+      "return": 103.0,
       "type": "current"
     },
     {
-      "code": "2327",
-      "name": "國巨*",
-      "return": 3200.0,
+      "code": "2891",
+      "name": "中信金",
+      "return": 86.9,
       "type": "current"
     },
     {
-      "code": "1303",
-      "name": "南亞",
-      "return": 186.3,
-      "type": "current"
-    },
-    {
-      "code": "1216",
-      "name": "統一",
-      "return": 562.0,
-      "type": "current"
-    },
-    {
-      "code": "6669",
-      "name": "緯穎",
-      "return": 1152.7,
-      "type": "current"
-    },
-    {
-      "code": "3653",
-      "name": "健策",
-      "return": 4030.8,
-      "type": "current"
-    },
-    {
-      "code": "2880",
-      "name": "華南金",
-      "return": 38.3,
-      "type": "current"
-    },
-    {
-      "code": "2892",
-      "name": "第一金",
-      "return": 23.0,
-      "type": "current"
-    },
-    {
-      "code": "2883",
-      "name": "凱基金",
-      "return": 53.2,
-      "type": "current"
-    },
-    {
-      "code": "2368",
-      "name": "金像電",
-      "return": 23066.7,
-      "type": "current"
-    },
-    {
-      "code": "2449",
-      "name": "京元電子",
-      "return": 1721.7,
-      "type": "current"
-    },
-    {
-      "code": "2344",
-      "name": "華邦電",
-      "return": 488.0,
-      "type": "current"
-    },
-    {
-      "code": "2301",
-      "name": "光寶科",
-      "return": 333.5,
-      "type": "current"
-    },
-    {
-      "code": "5880",
-      "name": "合庫金",
-      "return": 19.2,
-      "type": "current"
-    },
-    {
-      "code": "2408",
-      "name": "南亞科",
-      "return": 895.2,
-      "type": "current"
-    },
-    {
-      "code": "2603",
-      "name": "長榮",
-      "return": 832.9,
-      "type": "current"
-    },
-    {
-      "code": "2002",
-      "name": "中鋼",
-      "return": -2.1,
-      "type": "current"
-    },
-    {
-      "code": "3008",
-      "name": "大立光",
-      "return": 1199.0,
-      "type": "current"
-    },
-    {
-      "code": "3661",
-      "name": "世芯-KY",
-      "return": 4689.8,
+      "code": "2317",
+      "name": "鴻海",
+      "return": 84.6,
       "type": "current"
     },
     {
@@ -260,45 +254,33 @@ const ANALYSIS_DATA = {
       "type": "current"
     },
     {
-      "code": "1301",
-      "name": "台塑",
-      "return": 10.1,
+      "code": "2883",
+      "name": "凱基金",
+      "return": 53.2,
       "type": "current"
     },
     {
-      "code": "2059",
-      "name": "川湖",
-      "return": 3258.4,
+      "code": "2880",
+      "name": "華南金",
+      "return": 38.3,
       "type": "current"
     },
     {
-      "code": "4904",
-      "name": "遠傳",
-      "return": 274.4,
+      "code": "2887",
+      "name": "台新新光金",
+      "return": 34.6,
       "type": "current"
     },
     {
-      "code": "3045",
-      "name": "台灣大",
-      "return": 289.7,
+      "code": "2892",
+      "name": "第一金",
+      "return": 23.0,
       "type": "current"
     },
     {
-      "code": "2395",
-      "name": "研華",
-      "return": 466.4,
-      "type": "current"
-    },
-    {
-      "code": "2207",
-      "name": "和泰車",
-      "return": 1637.7,
-      "type": "current"
-    },
-    {
-      "code": "6919",
-      "name": "康霈*",
-      "return": -81.4,
+      "code": "5880",
+      "name": "合庫金",
+      "return": 19.2,
       "type": "current"
     },
     {
@@ -306,85 +288,31 @@ const ANALYSIS_DATA = {
       "name": "台塑化",
       "return": 12.2,
       "type": "current"
+    },
+    {
+      "code": "1301",
+      "name": "台塑",
+      "return": 10.1,
+      "type": "current"
+    },
+    {
+      "code": "2002",
+      "name": "中鋼",
+      "return": -2.1,
+      "type": "current"
+    },
+    {
+      "code": "6919",
+      "name": "康霈*",
+      "return": -81.4,
+      "type": "current"
     }
   ],
   "departedStocks": [
     {
-      "code": "1326",
-      "name": "台化",
-      "return": 34.1,
-      "type": "departed"
-    },
-    {
-      "code": "1402",
-      "name": "遠東新世紀",
-      "return": 121.6,
-      "type": "departed"
-    },
-    {
-      "code": "2105",
-      "name": "正新橡膠",
-      "return": -22.7,
-      "type": "departed"
-    },
-    {
-      "code": "2201",
-      "name": "裕隆汽車",
-      "return": -32.1,
-      "type": "departed"
-    },
-    {
-      "code": "2204",
-      "name": "中華汽車",
-      "return": -16.2,
-      "type": "departed"
-    },
-    {
-      "code": "2323",
-      "name": "中環",
-      "return": -26.5,
-      "type": "departed"
-    },
-    {
-      "code": "2324",
-      "name": "仁寶電腦",
-      "return": -15.8,
-      "type": "departed"
-    },
-    {
-      "code": "2325",
-      "name": "矽品精密",
-      "return": -100.0,
-      "type": "departed"
-    },
-    {
       "code": "2337",
       "name": "旺宏電子",
       "return": 1122.2,
-      "type": "departed"
-    },
-    {
-      "code": "2352",
-      "name": "佳世達",
-      "return": -36.8,
-      "type": "departed"
-    },
-    {
-      "code": "2353",
-      "name": "宏碁",
-      "return": -12.8,
-      "type": "departed"
-    },
-    {
-      "code": "2356",
-      "name": "英業達",
-      "return": 139.3,
-      "type": "departed"
-    },
-    {
-      "code": "2377",
-      "name": "微星科技",
-      "return": 66.8,
       "type": "departed"
     },
     {
@@ -394,33 +322,75 @@ const ANALYSIS_DATA = {
       "type": "departed"
     },
     {
+      "code": "2912",
+      "name": "統一超商",
+      "return": 334.3,
+      "type": "departed"
+    },
+    {
+      "code": "3034",
+      "name": "聯詠",
+      "return": 182.4,
+      "type": "departed"
+    },
+    {
+      "code": "2356",
+      "name": "英業達",
+      "return": 139.3,
+      "type": "departed"
+    },
+    {
+      "code": "1402",
+      "name": "遠東新世紀",
+      "return": 121.6,
+      "type": "departed"
+    },
+    {
       "code": "2388",
       "name": "威盛電子",
       "return": 108.5,
       "type": "departed"
     },
     {
-      "code": "2401",
-      "name": "凌陽科技",
-      "return": -60.8,
+      "code": "3665",
+      "name": "貿聯-KY",
+      "return": 73.0,
       "type": "departed"
     },
     {
-      "code": "2409",
-      "name": "友達光電",
-      "return": -15.7,
+      "code": "2377",
+      "name": "微星科技",
+      "return": 66.8,
       "type": "departed"
     },
     {
-      "code": "2475",
-      "name": "中華映管",
-      "return": -100.0,
+      "code": "2609",
+      "name": "陽明",
+      "return": 65.3,
       "type": "departed"
     },
     {
-      "code": "2610",
-      "name": "中華航空",
-      "return": 20.2,
+      "code": "4938",
+      "name": "和碩",
+      "return": 63.3,
+      "type": "departed"
+    },
+    {
+      "code": "3697",
+      "name": "晨星-KY(已下市)",
+      "return": 52.3,
+      "type": "departed"
+    },
+    {
+      "code": "1326",
+      "name": "台化",
+      "return": 34.1,
+      "type": "departed"
+    },
+    {
+      "code": "1722",
+      "name": "台肥",
+      "return": 29.3,
       "type": "departed"
     },
     {
@@ -430,9 +400,243 @@ const ANALYSIS_DATA = {
       "type": "departed"
     },
     {
-      "code": "2912",
-      "name": "統一超商",
-      "return": 334.3,
+      "code": "2633",
+      "name": "台灣高鐵",
+      "return": 26.0,
+      "type": "departed"
+    },
+    {
+      "code": "2610",
+      "name": "中華航空",
+      "return": 20.2,
+      "type": "departed"
+    },
+    {
+      "code": "8046",
+      "name": "南電",
+      "return": 11.8,
+      "type": "departed"
+    },
+    {
+      "code": "5871",
+      "name": "中租-KY",
+      "return": 8.9,
+      "type": "departed"
+    },
+    {
+      "code": "5876",
+      "name": "上海商銀",
+      "return": 1.0,
+      "type": "departed"
+    },
+    {
+      "code": "1802",
+      "name": "台玻",
+      "return": -3.8,
+      "type": "departed"
+    },
+    {
+      "code": "6004",
+      "name": "元大證(已下市)",
+      "return": -4.6,
+      "type": "departed"
+    },
+    {
+      "code": "1102",
+      "name": "亞泥",
+      "return": -6.5,
+      "type": "departed"
+    },
+    {
+      "code": "9904",
+      "name": "寶成工業",
+      "return": -10.1,
+      "type": "departed"
+    },
+    {
+      "code": "2353",
+      "name": "宏碁",
+      "return": -12.8,
+      "type": "departed"
+    },
+    {
+      "code": "2409",
+      "name": "友達光電",
+      "return": -15.7,
+      "type": "departed"
+    },
+    {
+      "code": "2324",
+      "name": "仁寶電腦",
+      "return": -15.8,
+      "type": "departed"
+    },
+    {
+      "code": "2204",
+      "name": "中華汽車",
+      "return": -16.2,
+      "type": "departed"
+    },
+    {
+      "code": "1590",
+      "name": "亞德客-KY",
+      "return": -17.3,
+      "type": "departed"
+    },
+    {
+      "code": "6116",
+      "name": "彩晶(已下市)",
+      "return": -19.7,
+      "type": "departed"
+    },
+    {
+      "code": "2105",
+      "name": "正新橡膠",
+      "return": -22.7,
+      "type": "departed"
+    },
+    {
+      "code": "6446",
+      "name": "藥華藥",
+      "return": -23.6,
+      "type": "departed"
+    },
+    {
+      "code": "2323",
+      "name": "中環",
+      "return": -26.5,
+      "type": "departed"
+    },
+    {
+      "code": "2371",
+      "name": "大同",
+      "return": -26.7,
+      "type": "departed"
+    },
+    {
+      "code": "3012",
+      "name": "廣輝電子(已下市)",
+      "return": -27.3,
+      "type": "departed"
+    },
+    {
+      "code": "9910",
+      "name": "豐泰",
+      "return": -31.8,
+      "type": "departed"
+    },
+    {
+      "code": "2201",
+      "name": "裕隆汽車",
+      "return": -32.1,
+      "type": "departed"
+    },
+    {
+      "code": "1476",
+      "name": "儒鴻",
+      "return": -34.4,
+      "type": "departed"
+    },
+    {
+      "code": "1605",
+      "name": "華新麗華",
+      "return": -36.4,
+      "type": "departed"
+    },
+    {
+      "code": "2352",
+      "name": "佳世達",
+      "return": -36.8,
+      "type": "departed"
+    },
+    {
+      "code": "2448",
+      "name": "晶電(已下市)",
+      "return": -39.6,
+      "type": "departed"
+    },
+    {
+      "code": "2474",
+      "name": "可成",
+      "return": -40.9,
+      "type": "departed"
+    },
+    {
+      "code": "2347",
+      "name": "聯強",
+      "return": -41.6,
+      "type": "departed"
+    },
+    {
+      "code": "2349",
+      "name": "錸德",
+      "return": -43.8,
+      "type": "departed"
+    },
+    {
+      "code": "1101",
+      "name": "台泥",
+      "return": -48.4,
+      "type": "departed"
+    },
+    {
+      "code": "2227",
+      "name": "裕日車",
+      "return": -53.4,
+      "type": "departed"
+    },
+    {
+      "code": "3673",
+      "name": "TPK-KY(已下市)",
+      "return": -55.4,
+      "type": "departed"
+    },
+    {
+      "code": "2354",
+      "name": "鴻準",
+      "return": -59.5,
+      "type": "departed"
+    },
+    {
+      "code": "8454",
+      "name": "富邦媒",
+      "return": -60.5,
+      "type": "departed"
+    },
+    {
+      "code": "2401",
+      "name": "凌陽科技",
+      "return": -60.8,
+      "type": "departed"
+    },
+    {
+      "code": "2615",
+      "name": "萬海",
+      "return": -62.4,
+      "type": "departed"
+    },
+    {
+      "code": "2888",
+      "name": "新光金(已下市)",
+      "return": -71.2,
+      "type": "departed"
+    },
+    {
+      "code": "6415",
+      "name": "矽力-KY",
+      "return": -79.3,
+      "type": "departed"
+    },
+    {
+      "code": "2498",
+      "name": "宏達電",
+      "return": -87.3,
+      "type": "departed"
+    },
+    {
+      "code": "3481",
+      "name": "群創",
+      "return": -92.4,
       "type": "departed"
     },
     {
@@ -442,9 +646,15 @@ const ANALYSIS_DATA = {
       "type": "departed"
     },
     {
-      "code": "9904",
-      "name": "寶成工業",
-      "return": -10.1,
+      "code": "2325",
+      "name": "矽品精密",
+      "return": -100.0,
+      "type": "departed"
+    },
+    {
+      "code": "2475",
+      "name": "中華映管",
+      "return": -100.0,
       "type": "departed"
     }
   ]
